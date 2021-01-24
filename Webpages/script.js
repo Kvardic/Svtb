e103ccd312727c99162de3f986409acfa3531c1a
Kvardic <43748137+Kvardic@users.noreply.github.com>
2021-01-24 18:56:41 +0100

    Mupdate_1

---
+++ Webpages/script.js
@@ -0,0 +1,81 @@
+(function($) {
+  var selector = ".extent";
+  var direction;
+  var $slide;
+  var offsetTop;
+  var $slides = $(selector);
+  var currentSlide = 0;
+  var isAnimating = false;
+
+  var stopAnimation = function() {
+    setTimeout(function() {
+      isAnimating = false;
+    }, 300);
+  };
+
+  var bottomIsReached = function($elem) {
+    var rect = $elem[0].getBoundingClientRect();
+    return rect.bottom <= $(window).height();
+  };
+
+  var topIsReached = function($elem) {
+    var rect = $elem[0].getBoundingClientRect();
+    return rect.top >= 0;
+  };
+
+  document.addEventListener(
+    "wheel",
+    function(event) {
+      var $currentSlide = $($slides[currentSlide]);
+
+      if (isAnimating) {
+        event.preventDefault();
+        return;
+      }
+
+      direction = -event.deltaY;
+
+      if (direction < 0) {
+        // next
+        if (currentSlide + 1 >= $slides.length) {
+          return;
+        }
+        if (!bottomIsReached($currentSlide)) {
+          return;
+        }
+        event.preventDefault();
+        currentSlide++;
+        $slide = $($slides[currentSlide]);
+        offsetTop = $slide.offset().top;
+        isAnimating = true;
+        $("html, body").animate({
+            scrollTop: offsetTop
+          },
+          1000,
+          stopAnimation
+        );
+      } else {
+        // back
+        if (currentSlide - 1 < 0) {
+          return;
+        }
+        if (!topIsReached($currentSlide)) {
+          return;
+        }
+        event.preventDefault();
+        currentSlide--;
+        $slide = $($slides[currentSlide]);
+        offsetTop = $slide.offset().top;
+        isAnimating = true;
+        $("html, body").animate({
+            scrollTop: offsetTop
+          },
+          1000,
+          stopAnimation
+        );
+      }
+    }, {
+      passive: false
+    }
+  );
+})(jQuery);
